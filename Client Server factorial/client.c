9a039f286d76230dc6d702cc4a01fa6a1cb7c510
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2023-03-28 15:39:37 +0530

    Update client.c

--- Client Server factorial/client.c
+++ Client Server factorial/client.c
@@ -4,12 +4,16 @@
 #include<string.h>
 #include<stdlib.h>
 #include <unistd.h>
+#define MAX 80
+#define port 8080
+
 int main()
 {
 	int k; 
 	int sock_desc;
-	char buf[100];
-	struct sockaddr_in client;
+	char buff[100];
+	int n;
+	struct sockaddr_in server,client;
 	sock_desc = socket(AF_INET,SOCK_STREAM,0);//AF_INET to provide interprocess communication between process that run on same system or different systems
 	
 	if(sock_desc == -1)
@@ -19,7 +23,12 @@ int main()
 	
 	client.sin_family = AF_INET;		//sin_family contains address family and is AF_INET when TCP/UDP is used
 	client.sin_addr.s_addr = INADDR_ANY;	//sin.addr contains IP Address
-	client.sin_port = 3003;
+	client.sin_port = htons(port);
+	
+	bzero(&server,sizeof(server));		//erases the data
+	server.sin_family = AF_INET;
+	server.sin_addr.s_addr = inet_addr("127.0.0.1");
+	server.sin_port = htons(port);
 	
 	k = connect(sock_desc,(struct sockaddr*) &client, sizeof(client));
 	
@@ -27,17 +36,31 @@ int main()
 	{
 		printf("Error in connecting to server...");
 	}
+	else
+		printf("Connected to server...");
 	
-	printf("Enter the data to be sent : ");
-	fgets(buf,100,stdin);
 	
-	k = send(sock_desc,buf,100,0);
-	
-	if(k == -1)
+	for(;;)
 	{
-		printf("Error in sending data to the server...");
+		bzero(buff,sizeof(buff));		//erases the data
+		printf("\nEnter the data to be sent : ");
+		int n=0;
+		while((buff[n++]=getchar())!='\n');
+		write(sock_desc,buff,sizeof(buff));
+		if(strncmp("exit",buff,4)==0)
+		{
+		printf("\nclient exits\n");
+		break;
+		}
+		bzero(buff,sizeof(buff));	
+		read(sock_desc,buff,sizeof(buff));
+		if(strncmp("exit",buff,4)==0)
+		{
+		printf("\nClient exits\n");
+		break;
+		}
+		printf("\nFrom server:%s\n",buff);
 	}
-	//printf("Data Sent!!!");
 	close(sock_desc);
 	return 0;
 	
