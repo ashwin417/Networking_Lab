0a87389678d7273a743a8c1b7d4a71d4643bf105
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2023-06-06 15:40:14 +0530

    Create client.c

---
+++ Sliding_Window_Protocols/Goback/client.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <netinet/in.h>
+#include <sys/time.h>
+#include <sys/wait.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+int main()
+{
+    int c_sock;
+    c_sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in client;
+    memset(&client, 0, sizeof(client));
+    client.sin_family = AF_INET;
+    client.sin_port = htons(9009);
+    client.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+    if (connect(c_sock, (struct sockaddr *)&client, sizeof(client)) == -1)
+    {
+        printf("Connection failed");
+        return 0;
+    }
+    printf("\n\tClient -with individual acknowledgement scheme\n\n");
+    char msg1[50] = "akwnowledgementof-";
+    char msg2[50];
+    char buff[100];
+    int flag = 1, flg = 1;
+    for (int i = 0; i <= 9; i++)
+    {
+        flg = 1;
+        bzero(buff, sizeof(buff));
+        bzero(msg2, sizeof(msg2));
+        if (i == 8 && flag == 1)
+        {
+            printf("here\n");
+            flag = 0;
+            read(c_sock, buff, sizeof(buff));
+        }
+        int n = read(c_sock, buff, sizeof(buff));
+        if (buff[strlen(buff) - 1] != i + '0')
+        {
+            printf("Discarded as out of order \n");
+            i--;
+        }
+        else
+        {
+            printf("Message received from server : %s \n", buff);
+            printf("Aknowledgement sent for message \n");
+            strcpy(msg2, msg1);
+            msg2[strlen(msg2)] = i + '0';
+            write(c_sock, msg2, sizeof(msg2));
+        }
+    }
+
+    close(c_sock);
+    return 0;
+}
