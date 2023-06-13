93beb8dfda7e9987bf9ed7f268e6e12d13ef480a
R Ashwin <77284634+ashwin417@users.noreply.github.com>
2023-06-13 14:47:17 +0530

    Added Working FTP program

---
+++ FTP/client.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define SERVER_IP "127.0.0.1"
+#define PORT 8080
+#define MAX_BUFFER_SIZE 1024
+
+int main() {
+    int socket_desc;
+    struct sockaddr_in server;
+    char buffer[MAX_BUFFER_SIZE];
+
+    // Create socket
+    socket_desc = socket(AF_INET, SOCK_STREAM, 0);
+    if (socket_desc == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr(SERVER_IP);
+    server.sin_port = htons(PORT);
+
+    // Connect to server
+    if (connect(socket_desc, (struct sockaddr*)&server, sizeof(server)) < 0) {
+        perror("Connection failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connected to FTP server at %s:%d\n", SERVER_IP, PORT);
+
+    // Send file to server
+    FILE* file = fopen("file_to_send.txt", "r");
+    if (!file) {
+        perror("File opening failed");
+        exit(EXIT_FAILURE);
+    }
+
+    while (!feof(file)) {
+        size_t read_size = fread(buffer, sizeof(char), sizeof(buffer), file);
+        if (send(socket_desc, buffer, read_size, 0) < 0) {
+            perror("Send failed");
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    fclose(file);
+    printf("File sent successfully\n");
+
+    // Close the connection
+    close(socket_desc);
+    return 0;
+}
---
+++ FTP/server.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define MAX_BUFFER_SIZE 1024
+
+int main() {
+    int server_socket, client_socket, read_size;
+    struct sockaddr_in server, client;
+    char buffer[MAX_BUFFER_SIZE];
+
+    // Create socket
+    server_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_socket == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(PORT);
+
+    // Bind
+    if (bind(server_socket, (struct sockaddr*)&server, sizeof(server)) < 0) {
+        perror("Binding failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Listen
+    listen(server_socket, 3);
+
+    printf("FTP server listening on port %d...\n", PORT);
+
+    // Accept and handle incoming connections
+    while (1) {
+        // Accept connection
+        int client_address_size = sizeof(struct sockaddr_in);
+        client_socket = accept(server_socket, (struct sockaddr*)&client, (socklen_t*)&client_address_size);
+        if (client_socket < 0) {
+            perror("Accept failed");
+            exit(EXIT_FAILURE);
+        }
+
+        printf("Connected to client: %s:%d\n", inet_ntoa(client.sin_addr), ntohs(client.sin_port));
+
+        // Receive file from client
+        FILE* file = fopen("received_file.txt", "w");
+        if (!file) {
+            perror("File opening failed");
+            exit(EXIT_FAILURE);
+        }
+
+        while ((read_size = recv(client_socket, buffer, sizeof(buffer), 0)) > 0) {
+            fwrite(buffer, sizeof(char), read_size, file);
+        }
+
+        fclose(file);
+        printf("File received and saved as 'received_file.txt'\n");
+
+        // Close the connection
+        close(client_socket);
+    }
+
+    close(server_socket);
+    return 0;
+}
