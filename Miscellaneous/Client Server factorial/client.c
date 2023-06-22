5ec88410751e4942891ef45d8dff4fefd2b84461
ashwin417 <ashwinr417@yahoo.com>
2023-06-22 11:25:57 +0530

    modified:   Miscellaneous/Client Server factorial/client 	modified:   Miscellaneous/Client Server factorial/client.c 	modified:   Miscellaneous/Client Server factorial/server 	modified:   Miscellaneous/Client Server factorial/server.c

--- Miscellaneous/Client Server factorial/client.c
+++ Miscellaneous/Client Server factorial/client.c
@@ -1,4 +1,65 @@
-#include<stdio.h>
+#include <stdio.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define MAX 80
+#define PORT 8888
+
+int main() {
+    int k;
+    int sock_desc;
+    char buff[MAX];
+    struct sockaddr_in server;
+
+    sock_desc = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sock_desc == -1) {
+        printf("Error in Socket Creation!!!");
+        return 1;
+    }
+
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_port = htons(PORT);
+
+    k = connect(sock_desc, (struct sockaddr*) &server, sizeof(server));
+
+    if (k == -1) {
+        printf("Error in connecting to server...\n");
+        return 1;
+    } else {
+        printf("Connected to server...\n");
+    }
+
+    while (1) {
+        printf("Enter the data to be sent: ");
+        fgets(buff, sizeof(buff), stdin);
+
+        // Remove trailing newline character
+        buff[strcspn(buff, "\n")] = '\0';
+
+        write(sock_desc, buff, strlen(buff) + 1);
+
+        if (strncmp("exit", buff, 4) == 0) {
+            printf("Client requested exit\n");
+            break;
+        }
+
+        bzero(buff, sizeof(buff));
+        read(sock_desc, buff, sizeof(buff));
+
+        printf("From server: %s\n", buff);
+    }
+
+    close(sock_desc);
+    return 0;
+}
+
+
+
+/* #include<stdio.h>
 #include<netinet/in.h> 		//Contains definitions of IP
 #include<stdio.h>
 #include<string.h>
@@ -66,3 +127,4 @@ int main()
 	
 	
 }
+*/
--- Miscellaneous/Client Server factorial/server.c
+++ Miscellaneous/Client Server factorial/server.c
@@ -1,5 +1,94 @@
+#include <stdio.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
 
-#include<netinet/in.h> 		//Contains definitions of IP
+#define MAX 80
+#define port 8888
+
+int factorial(int n) {
+    if (n == 0 || n == 1)
+        return 1;
+    else
+        return n * factorial(n - 1);
+}
+
+int main() {
+    char buff[MAX];
+    int k;
+    socklen_t len;
+    int sock_desc, temp_sock_desc;
+    struct sockaddr_in server, client;
+    sock_desc = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sock_desc == -1) {
+        printf("Error in Socket Creation!!!");
+        return 1; // Exit with an error code
+    } else {
+        printf("Socket successfully created!\n");
+    }
+
+    bzero(&server, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port);
+
+    k = bind(sock_desc, (struct sockaddr*) &server, sizeof(server));
+
+    if (k == -1) {
+        printf("Error in binding...\n");
+        return 1; // Exit with an error code
+    } else {
+        printf("Binded Successfully\n");
+    }
+
+    k = listen(sock_desc, 5);
+
+    if (k == -1) {
+        printf("Error in listening\n");
+        return 1; // Exit with an error code
+    }
+
+    len = sizeof(client);
+    temp_sock_desc = accept(sock_desc, (struct sockaddr*) &client, &len);
+
+    if (temp_sock_desc == -1) {
+        printf("Error in Temporary socket creation!\n");
+        return 1; // Exit with an error code
+    }
+
+    for (;;) {
+        bzero(buff, MAX);
+        read(temp_sock_desc, buff, sizeof(buff));
+        if (strncmp(buff, "exit", 4) == 0) {
+            printf("Exiting\n");
+            break;
+        }
+
+        int n = atoi(buff);
+        int fact = factorial(n);
+
+        bzero(buff, MAX);
+        printf("Factorial of the number sent from the client is: %d\n", fact);
+        sprintf(buff, "%d", fact);
+        write(temp_sock_desc, buff, sizeof(buff));
+        if (strncmp("exit", buff, 4) == 0) {
+            printf("Server exits\n");
+            break;
+        }
+    }
+
+    close(temp_sock_desc);
+    close(sock_desc);
+
+    return 0;
+}
+
+
+/* #include<netinet/in.h> 		//Contains definitions of IP
 #include<stdio.h>
 #include<string.h>
 #include<stdlib.h>
@@ -61,7 +150,7 @@ int main()
 	if(k==-1)
 	{
 		printf("Error in recieving");
-	}*/
+	}
 	int x;
 	for(;;)
 	{
@@ -69,7 +158,91 @@ int main()
 		read(temp_sock_desc,buff,sizeof(buff));
 		if(strncmp(buff,"exit",4)==0)
 		{
-			printf("Exitting");
+			#include <stdio.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#define MAX 80
+#define port 8080
+
+int main() {
+    char buff[MAX];
+    int k;
+    socklen_t len;
+    int sock_desc, temp_sock_desc;
+    struct sockaddr_in server, client;
+    sock_desc = socket(AF_INET, SOCK_STREAM, 0);
+
+    if (sock_desc == -1) {
+        printf("Error in Socket Creation!!!");
+        return 1; // Exit with an error code
+    } else {
+        printf("Socket successfully created!\n");
+    }
+
+    bzero(&server, sizeof(server));
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons(port);
+
+    k = bind(sock_desc, (struct sockaddr*) &server, sizeof(server));
+
+    if (k == -1) {
+        printf("Error in binding...\n");
+        return 1; // Exit with an error code
+    } else {
+        printf("Binded Successfully\n");
+    }
+
+    k = listen(sock_desc, 5);
+
+    if (k == -1) {
+        printf("Error in listening\n");
+        return 1; // Exit with an error code
+    }
+
+    len = sizeof(client);
+    temp_sock_desc = accept(sock_desc, (struct sockaddr*) &client, &len);
+
+    if (temp_sock_desc == -1) {
+        printf("Error in Temporary socket creation!\n");
+        return 1; // Exit with an error code
+    }
+
+    for (;;) {
+        bzero(buff, MAX);
+        read(temp_sock_desc, buff, sizeof(buff));
+        if (strncmp(buff, "exit", 4) == 0) {
+            printf("Exiting\n");
+            break;
+        }
+
+        int n = atoi(buff);
+        int i, fact = 1;
+        for (i = 1; i <= n; i++) {
+            fact = fact * i;
+        }
+
+        bzero(buff, MAX);
+        printf("Factorial of the number sent from the client is: %d\n", fact);
+        sprintf(buff, "Factorial of the number sent from the client is: %d", fact);
+        write(temp_sock_desc, buff, sizeof(buff));
+        if (strncmp("exit", buff, 4) == 0) {
+            printf("Server exits\n");
+            break;
+        }
+    }
+
+    close(temp_sock_desc);
+    close(sock_desc);
+
+    return 0;
+}
+printf("Exitting");
 			break;
 		}
 		//printf("\nFrom client:%s\t To Client:",buff);
@@ -98,7 +271,7 @@ int main()
 	return 0;
 	
 
-}
+} */
 
 /*void itoa(int n, char s[])
  {
